6b355e478c86e53e7691f0be6614436280a4b936
harshmahalle <mahalleharsh9@gmail.com>
2024-05-23 20:26:58 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "john doe",
+    job: "Software Engineer",
+    image: "https://plus.unsplash.com/premium_photo-1688891564708-9b2247085923?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Annie Sierra",
+    job: "Graphic Designer",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqpIwU5-BH0xP3-8uQEKhDcZkSU-zCoyX7tP8cxlnnHg&s",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Holland Hess",
+    job: "Marketing Manager",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe85jCbFUfrK0Z0LhFDciK31lHZ5y7jvxY_8j8E5EwTA&s",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Michaela Avalos",
+    job: "Content Strategist",
+    image: "https://live.staticflickr.com/5252/5403292396_0804de9bcf_b.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Creed Mack",
+    job: "Data Analyst",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG9Jy5q6ie9vf5NhJeIVRg0Gr6jTrx74d7iy978nL11w&s",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
